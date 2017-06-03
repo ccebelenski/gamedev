431d3e3778112c525cb3c0d5585b038bab8d745f
ccebelenski <Czaeap#11>
2017-06-03 01:25:46 -0400

    Test class - sets up javafx window

--- src/com/gamedev/test/TestClass.scala
+++ src/com/gamedev/test/TestClass.scala
@@ -1,9 +1,23 @@
 package com.gamedev.test
 
-/**
-  * Created by ccebelenski on 6/1/2017.
-  */
-class TestClass {
+import javafx.application.Application
+import javafx.scene.Group
+import javafx.scene.Scene
+import javafx.stage.Stage
+import scala.collection.JavaConverters._
 
+object TestClass {
+  def main(args: Array[String]): Unit = {
+    Application.launch(classOf[TestClass],args:_*)
+  }
+}
 
+class TestClass extends Application {
+  @throws[Exception]
+  override def start(stage: Stage): Unit = {
+    val root = new Group
+    val scene = new Scene(root, 600, 400)
+    stage.setScene(scene)
+    stage.show()
+  }
 }
